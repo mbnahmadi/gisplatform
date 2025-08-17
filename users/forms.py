8cc5c633c21019b1c9dcf0c51b8663636895490e
mbnahmadi <mbn.ahmadi78@gmail.com>
2025-08-17 15:51:32 +0330

    (PASE 1): fix admin form to set_password

--- users/forms.py
+++ users/forms.py
@@ -13,12 +13,12 @@ class CustomUserCreationForm(forms.ModelForm):
         model = CustomUserModel
         fields = ('email', 'username')
 
-        def save(self, commit=True):
-            user = super().save(commit=False)
-            user.set_password(self.cleaned_data['password'])
-            if commit:
-                user.save()
-            return user
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
 
 
 
