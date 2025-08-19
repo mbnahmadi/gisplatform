c77f2f6959ba3c98656ab4e31fd8ff29930d9fc3
mbnahmadi <mbn.ahmadi78@gmail.com>
2025-08-19 19:37:22 +0330

    (PASE 1): fix verify user email attempts

--- core/verify_email.py
+++ core/verify_email.py
@@ -9,6 +9,7 @@ def verify_user_email(user, code, purpose):
     تلاش می‌کند آخرین OTP معتبر را قفل کند و بررسی نماید.
     اگر موفق باشد، OTP را مصرف می‌کند و رکورد otp را برمی‌گرداند.
     اگر ناموفق باشد ValidationError یا OTPCodeModel.DoesNotExist می‌رود بالا.
+    error_message باید خارج ترنزاکشن انجام بشن چون اگه raise بدیم ترنس اکشن rollback میکنه
     """
     # باعث میشخ یا این تراکنش کامل انجام بشه یا اصلا انجام نشه
     with transaction.atomic():
@@ -22,37 +23,37 @@ def verify_user_email(user, code, purpose):
 
         raw_code = code_qs.first() # اخرین کوئری بدست امده
         if not raw_code:
-            raise ValidationError("Invalid code or no code found.")
-
+            error_message = "Invalid code or no code found."
         # بررسی انقضا
-        if raw_code.is_expired():
-            raise ValidationError("Code expired.")
+        elif raw_code.is_expired():
+            error_message = "Code expired."
 
         # افزایش تعداد تلاش (atomic چون داخل تراکنش است)
         # با استفاده از F این افزایش مستقیم روی این فیلد اتفاق میوفته نه توی پایتون
         # raw_code.attempts = F('attempts') + 1
         # raw_code.save(update_fields=['attempts'])
+        else:
+            EmailCodeModel.objects.filter(pk=raw_code.pk).update(attempts=F('attempts') + 1)
 
-        EmailCodeModel.objects.filter(pk=raw_code.pk).update(attempts=F('attempts') + 1)
-        raw_code.refresh_from_db(fields=['attempts'])
+            # refresh از DB تا مقدار واقعی attempts را داشته باشیم
+            raw_code.refresh_from_db(fields=['attempts'])
 
-        # refresh از DB تا مقدار واقعی attempts را داشته باشیم
-        raw_code.refresh_from_db(fields=['attempts'])
-        # raw_code = EmailCodeModel.objects.get(pk=raw_code.pk)
-
-        # اگر از حد مجاز بیشتر شده باشد
-        max_attempts = getattr(settings, 'VERIFY_EMAIL_CODE_MAX_ATTEMPTS')
-        #getattr(settings, 'VERIFY_EMAIL_CODE_MAX_ATTEMPTS', 3) # از ستینگ بیا VERIFY_EMAIL_CODE_MAX_ATTEMPTS رو بگیر اگه نداشت 3 بذار
-        if raw_code.attempts > max_attempts:
-            raise ValidationError("Too many attempts. Please request a new code.")
+            # اگر از حد مجاز بیشتر شده باشد
+            max_attempts = getattr(settings, 'VERIFY_EMAIL_CODE_MAX_ATTEMPTS')
+            #getattr(settings, 'VERIFY_EMAIL_CODE_MAX_ATTEMPTS', 3) # از ستینگ بیا VERIFY_EMAIL_CODE_MAX_ATTEMPTS رو بگیر اگه نداشت 3 بذار
+            if raw_code.attempts > max_attempts:
+                error_message = "Too many attempts. Please request a new code."
 
         # بررسی کد
-        if not raw_code.check_code(code):
-            # کد اشتباه است ولی چون attempts افزایش یافته، تا رسیدن به حداکثر ادامه می‌یابد
-            raise ValidationError("Invalid code.")
+            elif not raw_code.check_code(code):
+                # کد اشتباه است ولی چون attempts افزایش یافته، تا رسیدن به حداکثر ادامه می‌یابد
+                error_message = "Invalid code."
 
-        
-        raw_code.is_used = True
-        raw_code.save(update_fields=['is_used'])  #update_fields فقط همین فیلد اپدیت میشه نه کل فیلد ها
+            else:
+                raw_code.is_used = True
+                raw_code.save(update_fields=['is_used'])  #update_fields فقط همین فیلد اپدیت میشه نه کل فیلد ها
+    # حالا خارج از atomic، اگر error بود پرتاب کن (تغییرات commit شدن)
+    if error_message:
+        raise ValidationError(error_message)
 
-        return raw_code
+    return raw_code
